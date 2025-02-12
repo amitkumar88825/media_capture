45112439fb5ff45170d795a2145d4e8aab2a1660
amit-kumar-yadav <amitkumaryadav.work@gmail.com>
2025-02-13 03:51:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,6 @@ app.use('*', (req, res) => {
 });
 
 // Server Connection
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, '0.0.0.0' ,() => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
