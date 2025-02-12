53be99a33073a3a421fd07e9700c79c0092820aa
amitkumar88825 <amitkumaryadav.work@gmail.com>
2025-02-13 05:10:47 +0530

    app is bug free

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,10 +3,11 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
     const token = req.header("Authorization");
     if (!token) return res.status(401).json({ message: "Access Denied" });
-
+    console.log(6 , token)
     try {
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified;
+        console.log(10 , verified);
         next();
     } catch (error) {
         res.status(400).json({ message: "Invalid Token" });
