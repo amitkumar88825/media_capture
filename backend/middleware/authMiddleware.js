0ede146597df37c23cd952aad43e4e7e98bd03d7
amitkumar88825 <amitkumaryadav.work@gmail.com>
2025-02-13 06:43:54 +0530

    app is bug free

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,7 +10,7 @@ const verifyToken = (req, res, next) => {
     try {
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified;
-        co
+        console.log(13 )
         console.log(10 , verified);
         next();
     } catch (error) {
