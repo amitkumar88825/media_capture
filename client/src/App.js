156e6a394a27ffbbf9c323a993b5369613310389
amitkumar88825 <amitkumaryadav.work@gmail.com>
2025-02-13 10:05:11 +0530

    security

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import './index.css';
 import Header from './components/navbar/Header';
 import Login from "./components/auth/Login";
@@ -8,10 +8,25 @@ import Media from "./components/media/Media";
 import NotFound from "./components/NotFound";
 import WelcomePage from "./components/WelcomePage";
 
-
 const App = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    // Allow access to /login and /signup without redirection
+    if (location.pathname !== "/login" && location.pathname !== "/signup") {
+      if (user && user.name && user._id) {
+        navigate("/media");
+      } else {
+        navigate("/");
+      }
+    }
+  }, [navigate, location.pathname]);
+
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
         <Route path="/login" element={<Login />} />
@@ -20,8 +35,15 @@ const App = () => {
         <Route path="/" element={<WelcomePage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </Router>
+    </>
   );
-}
+};
 
-export default App;
+// Wrapping App in Router to use useNavigate and useLocation
+const AppWrapper = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWrapper;
